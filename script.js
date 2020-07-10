da0d35c0fced9aca8edb4692ca2298e499e0f789
WhiteBlack <whiteblacks@yandex.ru>
2020-07-11 00:11:56 +0400

    add FPS

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let screenSize;
 let edgesRect;
 
+const FPSManager = {
+    interval: 1000,
+    lastTime: new Date(),
+    frames: 0,
+    count: 0,
+    update: function() {
+        this.count++;
+        const time = new Date();
+        if (time - this.lastTime > this.interval) {
+            this.frames = this.count;
+            this.count = 0;
+            this.lastTime = time;
+        }
+    }
+};
+
 const flocks = [];
 
 const settings = {
@@ -16,6 +32,7 @@ const settings = {
     separationFactor: 0.01,
     showPerception: false,
     showSeparation: false,
+    showFPS: false,
 };
 
 const gui = new dat.GUI();
@@ -40,6 +57,7 @@ function generateGUISettings() {
 
     gui.add(settings, 'showPerception');
     gui.add(settings, 'showSeparation');
+    gui.add(settings, 'showFPS');
 }
 
 function generateFlock(settings, color) {
@@ -66,15 +84,24 @@ function clearCanvas() {
     rect(0, 0, screenSize.width, screenSize.height);
 }
 
+function drawFPS() {
+    textSize(32);
+    fill('#ed225d');
+    text(FPSManager.frames, 10, 35);
+}
+
 function draw() {
     clearCanvas();
 
     flocks.forEach(flock => flock.draw());
+    settings.showFPS && drawFPS();
 
     update();
 }
 
 function update() {
+    FPSManager.update();
+
     edgesRect = {
         left: settings.edgeMargin,
         top: settings.edgeMargin,
